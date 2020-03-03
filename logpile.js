5335c600e8ebb66eefd4f3e8d531c94c9075b94d
Duncan Geere <radio.edit@gmail.com>
2020-03-03 12:34:33 +0100

    Better captions and code logic

--- logpile.js
+++ logpile.js
@@ -4,21 +4,20 @@
 
 // Get chart size
 const margin = {
-	top: 20,
+	top: 50,
 	right: 20,
 	bottom:0,
 	left: 100};
 
-const svg = d3.select("#chart")
-const width = +svg.attr("width");
-const height = +svg.attr("height");
-const innerWidth = width - margin.left - margin.right
-const innerHeight = height - margin.top - margin.bottom
-const gutter = 50; // gutter between charts
+const svg = d3.select("#chart"),
+	  height = +svg.attr("height"),
+	  innerHeight = height - margin.top - margin.bottom,
+	  gutter = 80, // gutter between charts
+	  circleSize = 35,
+	  labelOpacity = "8%";
 
 // Define my different scales
-const circleScale = d3.scaleSqrt().domain([0,1000000000000]).range ([1, 100]);
-
+const circleScale = d3.scaleSqrt().domain([0,1000000000000]).range ([20, 50]);
 
 const scales = [
 	{"name": "Kg",
@@ -46,22 +45,19 @@ for (let i=0; i < scales.length; i++) {
 // Main chart-drawing function
 function drawchart(error, data) {
 
-	// if (error) throw error;
-
 	// Save some repetition later.
 	for (i = 0; i < scales.length; i++) {
 		const chartHeight = innerHeight/scales.length;
 
 		const toPlot = scales[i].data;
 		const scale = scales[i].scale
-						.range([0, chartHeight - gutter]);
+			.range([0, chartHeight - gutter]);
 
 		// Add a group that'll contain the axis
-		svg.append("g")
+		const chart = svg.append("g")
 			.attr("transform", `translate(${margin.left},${margin.top + (i*chartHeight)})`)
-			.attr("id", `${scales[i].name}-chart`);
-
-		const chart = d3.select(`#${scales[i].name}-chart`);
+			.attr("id", `${scales[i].name}`)
+			.attr("class", "chart");
 
 		// Plot the axis
 		const axis = chart.append("g")
@@ -69,17 +65,16 @@ function drawchart(error, data) {
 			.attr("class", "axis")
 			.attr("id", `${scales[i].name}`);
 
+		console.log(scales[i].name);
+
 		// Label each chart with scale name
 		chart.append("text")
 			.text(scales[i].name)
-			.style("text-anchor","end")
-			.style("font-size", "70%")
+			.style("text-anchor","center")
+			.style("font-size", "450%")
 			.style("font-weight", "bold")
-			.attr("transform",`translate(-28,3)`);
-
-		// Debug logging - DELETE ME
-		console.log(scales[i].name);
-		console.log(toPlot);
+			.attr("transform", "rotate(90)")
+			.attr("opacity", labelOpacity);
 
 		// Plot the data
 		const circles = chart.selectAll("circle")
@@ -88,7 +83,18 @@ function drawchart(error, data) {
 			.append("circle")
 			.attr("cx", 0)
 			.attr("cy", d => scale(d.co2e))
-			.attr("r", 20);
+			.attr("r", circleSize);
+
+		// Plot some label lines
+		const lines = chart.append("g").attr("class", "labelline")
+			.selectAll(".labelline")
+			.data(toPlot)
+			.enter()
+			.append("line")
+			.attr("x1", circleSize)
+			.attr("y1", d => scale(d.co2e))
+			.attr("x2", circleSize + 12)
+			.attr("y2", d => scale(d.co2e));
 
 		// Plot the labels
 		const labels = chart.selectAll(".label")
@@ -96,12 +102,10 @@ function drawchart(error, data) {
 			.enter()
 			.append("text");
 
-		labels.attr("x", 50)
-			.attr("y", d => scale(d.co2e))
+		labels.attr("x", circleSize + 15)
+			.attr("y", d => scale(d.co2e) + 4)
 			.text(d => d.thing)
 			.style("font-size", "70%");
-
-		console.log(labels);
 	}
 
 };
@@ -113,9 +117,17 @@ d3.csv("data.csv").then(data => {
 
 	// Filter the data for each scale and store it in the scales object
 	for (let i=0; i < scales.length; i++) {
-		const filteredData = data.filter(datapoint => datapoint["scale"] == scales[i].name)
+		const filteredData = data.filter(datapoint => (datapoint["scale"] == scales[i].name) && (datapoint["display"] == "TRUE"));
 		scales[i].data = filteredData;
 	}
+
+	svg.append("text")
+		.text("CO2 in Tonnes")
+		.style("font-size", "70%")
+		.style("text-anchor", "end")
+		.style("font-weight", "bold")
+		.attr("x", margin.left)
+		.attr("y", margin.top - 12);
 
 	// Run the drawchart function
 	drawchart(data);
